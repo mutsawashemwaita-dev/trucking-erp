ee2a2c7b7e457da68a94a13582a164a613554154
mutsawashemwaita-dev <mutsawashemwaita@gmail.com>
2026-06-15 15:21:01 +0200

    fix deployment - PyMySQL and release command

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,14 +54,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='trucking_erp'),
+        'NAME': config('DB_NAME', default='railway'),
         'USER': config('DB_USER', default='root'),
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST': config('DB_HOST', default='localhost'),
         'PORT': config('DB_PORT', default='3306'),
         'OPTIONS': {
             'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
